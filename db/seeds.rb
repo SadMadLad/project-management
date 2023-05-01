3a26efcb7acd83996d8d4b76e4edda7aa50a1d44
Saad Azam <zeroinside4u@gmail.com>
2023-05-02 02:42:43 +0500

    Set up seeds for users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
+
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create({ first_name: 'New', last_name: 'User', email: 'new@user.com', password: 'password' })
+User.create({ first_name: 'NewTwo', last_name: 'UserTwo', email: 'new@user2.com',
+password: 'password' })
+User.create({ first_name: 'NewThree', last_name: 'UserThree', email: 'new@user3.com',
+password: 'password' })
+User.create({ first_name: 'NewFour', last_name: 'UserFour', email: 'new@user4.com',
+password: 'password' })
