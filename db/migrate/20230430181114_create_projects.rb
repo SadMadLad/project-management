92c04e69bab8d4aa673a188240994ba698dae957
Saad Azam <zeroinside4u@gmail.com>
2023-05-02 02:39:45 +0500

    Remove the user foreign key from Project and user UserCommunityMap model (through has_many)

--- db/migrate/20230430181114_create_projects.rb
+++ db/migrate/20230430181114_create_projects.rb
@@ -1,7 +1,6 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-      t.belongs_to :user, null: false, foreign_key: true
       t.string :title
 
       t.timestamps
