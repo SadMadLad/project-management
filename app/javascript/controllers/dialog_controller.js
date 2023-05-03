4938074b95d2a33d67d22a741ef21502ad66287e
Saad Azam <zeroinside4u@gmail.com>
2023-05-04 02:11:17 +0500

    Make a general dialog and use it for tasks#show and tasks#new

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -2,11 +2,18 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ 'dialog' ]
-  static values = { keep: Boolean }
+  static values = {
+    keep: { type: Boolean, default: false },
+    // If the dialog is only for a specific action to path.
+    actionOnly: { type: Boolean, default: true }
+  }
 
   dialogTargetConnected(element) { if(this.keepValue === true) { element.showModal() } }
 
-  close() { this.dialogTarget.close() }
+  close() {
+    this.dialogTarget.close()
+    if(this.actionOnlyValue === true) { this.dialogTarget.remove() }
+  }
   show() { this.dialogTarget.showModal() }
   stay(e) { e.stopPropagation() }
 }
