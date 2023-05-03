baf09178636a33fa5d5024e9c29825d3f4df885f
Saad Azam <zeroinside4u@gmail.com>
2023-05-03 21:29:21 +0500

    Remove form target as it is not needed.

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -1,19 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['form']
+  static targets = [ 'dialog' ]
   static values = { keep: Boolean }
 
-  connect() {
-    if(this.keepValue === true) { this.element.showModal() }
-  }
-  formTargetConnected(element) {
-    element.addEventListener('turbo:submit-end', e => {
-      if(e.detail.success) { this.element.close() }
-    })
-  }
+  dialogTargetConnected(element) { if(this.keepValue === true) { element.showModal() } }
 
-  close() { this.element.close() }
-
+  close() { this.dialogTarget.close() }
+  show() { this.dialogTarget.showModal() }
   stay(e) { e.stopPropagation() }
 }
