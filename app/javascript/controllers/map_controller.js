a1fcc3b426852862a66f86c8c83af6f519104e01
Saad Azam <zeroinside4u@gmail.com>
2023-05-04 02:41:46 +0500

    Incorporate map_controller for Form, Task and Project maps.

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -6,31 +6,38 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['map', 'latitude', 'longitude']
   static values = {
-    latitude: Number,
-    longitude: Number,
+    latitude: { type: Number, default: 31.313131 },
+    longitude: { type: Number, default: 70.31313 },
     isForm: { type: Boolean, default: false },
     isProject: { type: Boolean, default: false },
+    isTask: { type: Boolean, default: false },
     projectCoordinates: { type: Array, default: [] }
   }
 
-  ifIsProject(map) {
-    map.on('move', () => map.invalidateSize())
-
+  icon() {
     const icon = Leaflet.icon({
       iconUrl: '/marker-icon.png',
       shadowUrl: '/marker-shadow.png'
     })
 
+    return icon
+  }
+
+  // if it is a project map, loop over the tasks and render a marker for them with task's title as popup.
+  ifIsProject(map) {
+    map.on('move', () => map.invalidateSize())
+
     this.projectCoordinatesValue.forEach( taskInfo => {
       const [latitude, longitude, taskTitle] = taskInfo
-      const marker = L.marker([latitude, longitude], {icon: icon} ).bindPopup(taskTitle)
+      const marker = L.marker( [latitude, longitude], {icon: this.icon()} ).bindPopup(taskTitle)
       marker.addTo(map)
     })
   }
 
+  // if is a form, allow it to change the coordinates for saving it.
   ifIsForm(map) {
     const popup = Leaflet.popup()
-
+    const marker = Leaflet.marker([this.longitudeValue, this.latitudeValue], {icon: this.icon()} ).addTo(map)
     map.on('click', e => {
       popup
         .setLatLng(e.latlng)
@@ -38,11 +45,18 @@ export default class extends Controller {
         .openOn(map)
       this.latitudeTarget.value = e.latlng.lat
       this.longitudeTarget.value = e.latlng.lng
+      marker.setLatLng(e.latlng)
     })
   }
 
+  // if is a task, place the marker the specified latitude and longitude
+  ifIsTask(map) {
+    const marker = L.marker([this.latitudeValue, this.longitudeValue], {icon: this.icon()} )
+    marker.addTo(map)
+  }
+
   mapTargetConnected() {
-    const map = Leaflet.map(this.mapTarget).setView([this.latitudeValue, this.longitudeValue], 8)
+    const map = Leaflet.map(this.mapTarget).setView([this.latitudeValue, this.longitudeValue], 1.5)
 
     Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
@@ -51,5 +65,6 @@ export default class extends Controller {
 
     if (this.isProjectValue === true) { this.ifIsProject(map) }
     if (this.isFormValue === true) { this.ifIsForm(map) }
+    if (this.isTaskValue === true) { this.ifIsTask(map) }
   }
 }
