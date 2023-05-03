f0f9c91a5c3e85b63309b989d3211726597a5c60
Saad Azam <zeroinside4u@gmail.com>
2023-05-03 21:30:03 +0500

    Modify map_controller.js for displaying the location of projects on map.

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -2,12 +2,43 @@ import "leaflet-css"
 import * as Leaflet from "leaflet"
 import { Controller } from "@hotwired/stimulus"
 
+
 export default class extends Controller {
   static targets = ['map', 'latitude', 'longitude']
   static values = {
     latitude: Number,
     longitude: Number,
-    isForm: Boolean,
+    isForm: { type: Boolean, default: false },
+    isProject: { type: Boolean, default: false },
+    projectCoordinates: { type: Array, default: [] }
+  }
+
+  ifIsProject(map) {
+    map.on('move', () => map.invalidateSize())
+
+    const icon = Leaflet.icon({
+      iconUrl: '/marker-icon.png',
+      shadowUrl: '/marker-shadow.png'
+    })
+
+    this.projectCoordinatesValue.forEach( taskInfo => {
+      const [latitude, longitude, taskTitle] = taskInfo
+      const marker = L.marker([latitude, longitude], {icon: icon} ).bindPopup(taskTitle)
+      marker.addTo(map)
+    })
+  }
+
+  ifIsForm(map) {
+    const popup = Leaflet.popup()
+
+    map.on('click', e => {
+      popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map)
+      this.latitudeTarget.value = e.latlng.lat
+      this.longitudeTarget.value = e.latlng.lng
+    })
   }
 
   mapTargetConnected() {
@@ -18,17 +49,7 @@ export default class extends Controller {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map)
 
-    if(this.isFormValue === true) {
-      const popup = Leaflet.popup()
-
-      map.on('click', e => {
-        popup
-          .setLatLng(e.latlng)
-          .setContent("You clicked the map at " + e.latlng.toString())
-          .openOn(map)
-        this.latitudeTarget.value = e.latlng.lat
-        this.longitudeTarget.value = e.latlng.lng
-      })
-    }
+    if (this.isProjectValue === true) { this.ifIsProject(map) }
+    if (this.isFormValue === true) { this.ifIsForm(map) }
   }
 }
